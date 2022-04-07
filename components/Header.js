9b5c9383983ac159d9b0479b74aa4b82f8119daf
chege4179 <peterkagure@gmail.com>
2022-04-07 14:08:20 +0300

    Fixed some media queries

--- components/Header.js
+++ components/Header.js
@@ -40,13 +40,13 @@ const Header = () => {
 	}
 
 	return (
-		<div className='bg-white text-gray-200 shadow transition sticky z-20 top-0  border-b-2 border-gray-300 border-solid  w-full basis-16 flex items-center justify-center px-4 '>
-			<div className='max-w-screen-xl w-screen flex items-center justify-center'>
-				<div className='flex w-4/5 sm:w-3/5'>
-					<Link href='/' passHref>
-						<h1 className='text-indigo-500 text-center font-bold text-2xl hover:cursor-pointer hover:text-blue-700'>Blogify</h1>
+		<div className='bg-white text-gray-200 shadow transition sticky z-20 top-0  border-b-2 border-gray-300 border-solid  w-full basis-16 sm:basis-32 flex items-center justify-center px-4 '>
+			<div className='max-w-screen-xl w-screen flex sm:flex-col-reverse sm:w-full sm:h-full sm:justify-evenly items-center justify-center'>
+				<div className='flex w-4/5 sm:w-full sm:h-16 sm:h-full sm:flex sm: items-center'>
+					<Link href='/' passHref className="sm:hidden">
+						<h1 className='text-indigo-500 text-center font-bold sm:hidden text-2xl hover:cursor-pointer hover:text-blue-700'>Blogify</h1>
 					</Link>
-					<div className={`${router.pathname === '/post/new' && 'hidden'} bg-gray-100 p-1 border-2 border-solid border-gray-600 rounded-md w-1/2 mx-2 flex sm:hidden md:w-80`}>
+					<div className={`${router.pathname === '/post/new' && 'hidden'} bg-gray-100 p-1 border-2 border-solid border-gray-600 rounded-md w-1/2 mx-2 flex sm:h-3/4 sm:items-center sm:justify-center  md:w-80`}>
 						<input
 							value={searchTerm}
 							onChange={(e) => setSearchTerm(e.target.value)}
@@ -67,69 +67,77 @@ const Header = () => {
 						</button>
 					</div>
 				</div>
-				<div className='flex'>
-					{
-						user === null ? (
+				<div className='flex sm:1/2  sm:w-full justify-end sm:justify-evenly'>
+					<div className="w-1/2 flex justify-start xl:hidden">
+						<Link href='/' passHref >
+							<h1 className='text-indigo-500 text-center font-bold  text-2xl hover:cursor-pointer hover:text-blue-700'>Blogify</h1>
+						</Link>
+					</div>
+					<div className="flex">
+						{
+							user === null ? (
 
-							<>
-								<button
-									onClick={GoToLoginPage}
-									className="whitespace-nowrap relative mx-1 flex justify-center p-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:underline"
-								>
-									Log In
-								</button>
-								<button
-									onClick={GoToSignUpPage}
-									className="whitespace-nowrap group relative mx-1 flex justify-center p-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:underline">
-									Create Account
-								</button>
-							</>
-						) : (
-
-							<div className='flex '>
-								<h1 className='text-black sm:hidden pt-2 pr-2'>{user.name}</h1>
-								<div className=''>
-									<Image
-										src={generateAvatarURL(user?.name)} alt={user?.username} width={40}
-										height={40}
-										className='sm:w-8 sm:h-8   mx-1 rounded-2xl hover:cursor-pointer hover:border-black border-2 border-solid'
-										onClick={() => setShowDropdown(!showDropdown)}/>
-									<Popover
-										opened={showDropdown}
-										onClose={() => setShowDropdown(!showDropdown)}
-										width={210}
-										position="bottom"
+								<>
+									<button
+										onClick={GoToLoginPage}
+										className="whitespace-nowrap relative mx-1 flex justify-center p-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:underline"
 									>
-										<h2 className='block px-2 py-2 text-sm font-bold capitalize text-gray-700 hover:bg-blue-500 hover:text-white'>{user.name}</h2>
-										<h2 className='block px-2 py-2 text-sm font-bold text-gray-700 hover:bg-blue-500 hover:text-white'>{user.email}</h2>
-										<hr className='bg-black h-0.5'/>
-										<a href="#"
-										   className="block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
-											My Profile
-										</a>
-										<a href="#"
-										   className="block px-2 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
-											Create Post
-										</a>
-										<a href="#"
-										   className="block px-2 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
-											Settings
-										</a>
-										<a href="#" onClick={LogOut}
-										   className="block px-2 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
-											Sign Out
-										</a>
-									</Popover>
-								</div>
-								<button
-									onClick={GoToCreatePost}
-									className="whitespace-nowrap group relative mx-1 flex justify-center p-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:underline">
-									Create Post
-								</button>
+										Log In
+									</button>
+									<button
+										onClick={GoToSignUpPage}
+										className="whitespace-nowrap group relative mx-1 flex justify-center p-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:underline">
+										Create Account
+									</button>
+								</>
+							) : (
 
-							</div>
-						)
-					}
+								<div className='flex '>
+									<h1 className='text-black sm:hidden pt-2 pr-2'>{user.name}</h1>
+									<div className=''>
+										<Image
+											src={generateAvatarURL(user?.name)} alt={user?.username} width={40}
+											height={40}
+											className='sm:w-8 sm:h-8   mx-1 rounded-2xl hover:cursor-pointer hover:border-black border-2 border-solid'
+											onClick={() => setShowDropdown(!showDropdown)}/>
+										<Popover
+											opened={showDropdown}
+											onClose={() => setShowDropdown(!showDropdown)}
+											width={210}
+											position="bottom"
+										>
+											<h2 className='block px-2 py-2 text-sm font-bold capitalize text-gray-700 hover:bg-blue-500 hover:text-white'>{user.name}</h2>
+											<h2 className='block px-2 py-2 text-sm font-bold text-gray-700 hover:bg-blue-500 hover:text-white'>{user.email}</h2>
+											<hr className='bg-black h-0.5'/>
+											<a href="#"
+											   className="block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
+												My Profile
+											</a>
+											<a href="#"
+											   className="block px-2 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
+												Create Post
+											</a>
+											<a href="#"
+											   className="block px-2 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
+												Settings
+											</a>
+											<a href="#" onClick={LogOut}
+											   className="block px-2 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
+												Sign Out
+											</a>
+										</Popover>
+									</div>
+									<button
+										onClick={GoToCreatePost}
+										className="whitespace-nowrap group relative mx-1 flex justify-center p-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:underline">
+										Create Post
+									</button>
+
+								</div>
+							)
+						}
+					</div>
+
 				</div>
 			</div>
 		</div>
