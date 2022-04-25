65541938874309b4a72534a90b026958186c8e95
chege4179 <peterkagure@gmail.com>
2022-04-25 17:36:59 +0300

    Added some responsiveness

--- pages/post/search.js
+++ pages/post/search.js
@@ -24,10 +24,10 @@ const SearchPage = ({posts, users}) => {
 			<div className='justify-center w-full flex h-full overflow-y-hidden'>
 				<div className='max-w-screen-lg w-full flex justify-center '>
 					<div className='w-full h-full  justify-center p-3 mb-4'>
-						<div className='h-20 flex justify-between items-center px-3'>
-							<h1 className='w-3/5 font-bold text-4xl'>Search results for
+						<div className='h-20 flex justify-between items-center px-3 sm:flex-col '>
+							<h1 className='w-3/5 font-bold text-4xl sm:w-full sm:text-2xl'>Search results for
 								"{router.query.query}"</h1>
-							<div className='flex w-2/5 justify-end'>
+							<div className='flex w-2/5 justify-end sm:w-full sm:justify-evenly'>
 								<div className='h-10 w-24 flex justify-center items-center rounded  hover:bg-white hover:bg-indigo-100 hover:cursor-pointer'>
 									<h1 className=''>Newest</h1>
 								</div>
@@ -36,25 +36,25 @@ const SearchPage = ({posts, users}) => {
 								</div>
 							</div>
 						</div>
-						<div className='flex w-full h-full'>
-							<div className=' w-1/4 '>
-								<div className='w-full h-10  flex justify-start pl-2 items-center rounded mr-4 hover:bg-white hover:bg-indigo-100'
+						<div className='flex w-full h-full sm:flex-col '>
+							<div className='w-1/4 sm:w-full sm:flex sm:justify-evenly'>
+								<div className='w-full h-10  flex justify-start pl-2 items-center rounded mr-4 hover:bg-white hover:bg-indigo-100 sm:mr-0'
 									onClick={() => setSearchParams('posts')}>
 									<BsNewspaper className='mr-2'/>
 									<h1 className='font-bold text-lg'>Posts</h1>
 								</div>
-								<div className='w-full h-10  flex justify-start pl-2 items-center rounded mr-4 hover:bg-white hover:bg-indigo-100'
+								<div className='w-full h-10  flex justify-start pl-2 items-center rounded mr-4 hover:bg-white hover:bg-indigo-100 sm:mr-0'
 									onClick={() => setSearchParams('users')}>
 									<AiOutlineUser className='mr-2'/>
 									<h1 className='font-bold text-lg'>Users</h1>
 								</div>
-								<div className='w-full h-10  flex justify-start pl-2 items-center rounded mr-4 hover:bg-white hover:bg-indigo-100'
+								<div className='w-full h-10  flex justify-start pl-2 items-center rounded mr-4 hover:bg-white hover:bg-indigo-100 sm:mr-0'
 									onClick={() => setSearchParams('myposts')}>
 									<BsNewspaper className='mr-2'/>
 									<h1 className='font-bold text-lg'>My Posts</h1>
 								</div>
 							</div>
-							<div className='w-3/4 h-full flex flex-col px-1 overflow-y-scroll scrollbar-hide '>
+							<div className='w-3/4 h-full flex flex-col px-1 overflow-y-scroll scrollbar-hide sm:w-full'>
 								{
 									searchparams === 'posts' ? (
 										<div className='w-full h-full'>
---
+++ pages/trending.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from "../components/Layout";
+
+const TrendingPosts = () => {
+	return (
+		<Layout>
+			<div className='justify-center w-full flex h-full overflow-y-hidden'>
+				<div className='max-w-screen-xl w-full h-max flex justify-center bg-white '>
+					fjfjfjjf
+				</div>
+			</div>
+		</Layout>
+	);
+};
+
+export default TrendingPosts;
